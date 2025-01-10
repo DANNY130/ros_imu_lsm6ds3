327baad8052bef97be1015d8e33d9d53eca6938e
DANNY130 <phamd4@spu.edu>
2025-01-09 19:42:57 -0800

    update package name

--- launch/ros_imu_lsm6ds3.launch.py
+++ launch/ros_imu_lsm6ds3.launch.py
@@ -7,8 +7,8 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='ros_wiring_template',
-            executable='ros_wiring_template',
-            name='ros_wiring_template',
+            package='ros_imu_lsm6ds3',
+            executable='ros_imu_lsm6ds3',
+            name='ros_imu_lsm6ds3',
             output='screen'),
     ])
